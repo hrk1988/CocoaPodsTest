6a442c49dac70a2006517309a743dee8ba6c6432
HRK <h.r.k@163.com>
2015-06-11 17:49:47 +0800

    测试提交

---
+++ WaverTest/Waver/ViewController.m
@@ -0,0 +1,80 @@
+//
+//  ViewController.m
+//  Waver
+//
+//  Created by kevinzhow on 14/12/14.
+//  Copyright (c) 2014年 Catch Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Waver.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController ()
+
+@property (nonatomic, strong) AVAudioRecorder *recorder;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    [self setupRecorder];
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    Waver * waver = [[Waver alloc] initWithFrame:CGRectMake(0, CGRectGetHeight(self.view.bounds)/2.0 - 50.0, CGRectGetWidth(self.view.bounds), 100.0)];
+    
+    __weak Waver * weakWaver = waver;
+    waver.waverLevelCallback = ^() {
+        
+        [self.recorder updateMeters];
+        
+        CGFloat normalizedValue = pow (10, [self.recorder averagePowerForChannel:0] / 40);
+        
+        weakWaver.level = normalizedValue;
+        
+    };
+    [self.view addSubview:waver];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+-(void)setupRecorder
+{
+    NSURL *url = [NSURL fileURLWithPath:@"/dev/null"];
+    
+    NSDictionary *settings = @{AVSampleRateKey:          [NSNumber numberWithFloat: 44100.0],
+                               AVFormatIDKey:            [NSNumber numberWithInt: kAudioFormatAppleLossless],
+                               AVNumberOfChannelsKey:    [NSNumber numberWithInt: 2],
+                               AVEncoderAudioQualityKey: [NSNumber numberWithInt: AVAudioQualityMin]};
+    
+    NSError *error;
+    self.recorder = [[AVAudioRecorder alloc] initWithURL:url settings:settings error:&error];
+    
+    if(error) {
+        NSLog(@"Ups, could not create recorder %@", error);
+        return;
+    }
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
+    
+    if (error) {
+        NSLog(@"Error setting category: %@", [error description]);
+    }
+    
+    [self.recorder prepareToRecord];
+    [self.recorder setMeteringEnabled:YES];
+    [self.recorder record];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
